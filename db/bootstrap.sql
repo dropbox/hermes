cb9ec26fdedbc4d77e1c468aee187762b53077c2
Digant C Kasundra <digant@dropbox.com>
2015-05-18 07:52:49 -0700

    Added event-type and fate creation to CLI

--- db/bootstrap.sql
+++ db/bootstrap.sql
@@ -8,7 +8,8 @@ VALUES
 	(1,'system-reboot','required','This system requires a reboot.'),
 	(2,'system-reboot','completed','This system rebooted.'),
 	(3,'system-maintenance','required','This system requires maintenance.'),
-	(4,'system-maintenance','completed','System maintenance completed.');
+	(4,'system-maintenance','ready','This system is ready for maintenance.');
+	(5,'system-maintenance','completed','System maintenance completed.');
 
 UNLOCK TABLES;
 
@@ -20,6 +21,7 @@ LOCK TABLES `fates` WRITE;
 INSERT INTO `fates` (`id`, `creation_type_id`, `completion_type_id`, `description`)
 VALUES
 	(1,1,2,'A system that needs a reboot can be cleared by rebooting the machine.'),
-	(2,3,4,'A system that needs maintenance must be addressed and cleared by an SRE.');
+	(2,3,4,'A system that needs maintenance made ready before maintenance can occur.');
+	(3,4,5,'Maintenance must be performed on a system that is prepped.');
 
 UNLOCK TABLES;
